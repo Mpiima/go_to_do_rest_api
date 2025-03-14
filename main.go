e944a86f59d83fa78cdf6d9784c6a4c0e5538fab
Mpiima <collinsmpiima@gmail.com>
2025-03-14 10:44:22 +0300

    add todos

--- main.go
+++ main.go
@@ -7,9 +7,9 @@ import (
 )
 
 type todo struct {
-	ID        string
-	Item      string
-	Completed bool
+	ID        string `json:"id"`
+	Item      string `json:"item"`
+	Completed bool   `json:"completed"`
 }
 
 var todos = []todo{
@@ -22,8 +22,20 @@ func getTodos(context *gin.Context) {
 	context.IndentedJSON(http.StatusOK, todos)
 }
 
+func addTodo(context *gin.Context) {
+	var newTodo todo
+
+	if err := context.BindJSON(&newTodo); err != nil {
+		return
+	}
+
+	todos = append(todos, newTodo)
+	context.IndentedJSON(http.StatusCreated, newTodo)
+}
+
 func main() {
 	router := gin.Default()
 	router.GET("/todos", getTodos)
+	router.POST("/todos", addTodo)
 	router.Run("localhost:9090")
 }
